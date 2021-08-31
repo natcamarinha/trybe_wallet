d30930025f52c2d45fe254a71f966613fc0f862a
Natalia Camarinha <natcamarinha@gmail.com>
2021-08-31 19:33:30 -0300

    correção componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ExpenseForm from './ExpenseForm';
 import HeaderForm from './HeaderForm';
 
 class Header extends React.Component {
